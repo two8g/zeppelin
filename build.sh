ec4be2dab8d971fa93989e091a3b3ff5211585cf
two8g <ybxnice@gmai.com>
2018-10-29 19:28:49 +0800

    update

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-mvn package -DskipTests -Pspark-2.2 -Pscala-2.11 -Pbuild-distr
+mvn clean package -DskipTests -Pspark-2.2 -Pscala-2.11 -Phadoop-2.7 -Pbuild-distr -Dspark.version=2.2.1 -Dhadoop.version=2.7.6
+#-pl zeppelin-interpreter,zeppelin-zengine,spark,zeppelin-web,zeppelin-server,zeppelin-distribution,\
+
